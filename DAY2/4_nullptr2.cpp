38a406a0968daf036ca2c63e18d0f08b745f1e33
ecourse <smkang@codenuri.co.kr>
2022-01-25 11:08:16 +0900

    Add files via upload

--- DAY2/4_nullptr2.cpp
+++ DAY2/4_nullptr2.cpp
@@ -7,5 +7,15 @@ void g(char* n) { std::cout << "char*" << std::endl; }
 
 int main()
 {
-	f(0); // ?
+	f(0);		// 1.  0은 정수 입니다. int 타입   
+	f((void*)0);// 2. 
+
+#define NULL (void*)0
+
+	f(NULL);
+	g(NULL); // ? 잘생각해 보세요.
+			 // void* => char*  로의 암시적 형변환 필요
+			 // C 언어는 암시적 변환 허용
+			 // C++은 허용 안됨..
+			 // 그래서 현재는 C++이므로 에러.
 }
