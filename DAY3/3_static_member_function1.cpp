c287d7e74c052fa12d332aafadbf0135bb1ff2d9
ecourse <smkang@codenuri.co.kr>
2022-01-26 10:11:36 +0900

    Add files via upload

--- DAY3/3_static_member_function1.cpp
+++ DAY3/3_static_member_function1.cpp
@@ -15,19 +15,18 @@ public:
 
 	static int get_count() { return cnt; } // 정적(클래스) 메소드
 									// 객체없이 호출가능한 멤버함수(메소드)
+									// Car::get_count()로 호출
 };
 int Car::cnt = 0; // private에 있어도 외부 초기화는 허용됩니다.
 
-
 int main()
 {
-	std::cout << Car::cnt << std::endl;
+	std::cout << Car::get_count() << std::endl;
 
 	Car c1;
 	Car c2;
 
-
-	std::cout << Car::cnt << std::endl;
-	std::cout << c1.get_count() << std::endl;
+	std::cout << Car::get_count() << std::endl;
+	std::cout << c1.get_count()   << std::endl;
 
 }
