621187ddc90de14157f67ac9d7a0ea38d112d7d5
ecourse <smkang@codenuri.co.kr>
2022-01-26 10:09:59 +0900

    Add files via upload

---
+++ DAY3/3_static_member_function1.cpp
@@ -0,0 +1,33 @@
+// 3_static_member_function1 - 114page
+#include <iostream>
+
+class Car
+{
+	int speed;
+	static int cnt; 
+public:
+	Car() { ++cnt; }
+	~Car() { --cnt; }
+
+//	int get_count() { return cnt; } // 인스턴스 메소드 
+								    // 반드시 객체가 있어야 호출가능하다.
+									// c1.get_count()
+
+	static int get_count() { return cnt; } // 정적(클래스) 메소드
+									// 객체없이 호출가능한 멤버함수(메소드)
+};
+int Car::cnt = 0; // private에 있어도 외부 초기화는 허용됩니다.
+
+
+int main()
+{
+	std::cout << Car::cnt << std::endl;
+
+	Car c1;
+	Car c2;
+
+
+	std::cout << Car::cnt << std::endl;
+	std::cout << c1.get_count() << std::endl;
+
+}
