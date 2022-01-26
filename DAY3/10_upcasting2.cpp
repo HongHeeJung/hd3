477230b4b05e86157998dadd84b9287f0588b9d4
ecourse <smkang@codenuri.co.kr>
2022-01-26 16:32:54 +0900

    Add files via upload

--- DAY3/10_upcasting2.cpp
+++ DAY3/10_upcasting2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Animal
 {
@@ -11,12 +12,20 @@ class Cat : public Animal
 public:
 	int color;
 };
-void HappyNewYear(Dog* p)
-{
+//void HappyNewYear(Dog* p) // Dog만 전달 가능한 함수
+void HappyNewYear(Animal* p) // 동종(동일기반 클래스를 사용하는 )타입을 처리하는
+{							 // 함수 - 142 page
 	++(p->age);
 }
 int main()
 {
-	Dog d; HappyNewYear(&d);
-	Cat c; HappyNewYear(&c);
+	Dog d; HappyNewYear(&d); // ok
+	Cat c; HappyNewYear(&c); // ok
+
+
+	std::vector<Dog*> woori; // Dog 만 보관하는 우리
+
+	std::vector<Animal*> woori; // 모든 동물(동종)을 보관하는 컨테이너 만들기
+								// 142 아래부분
+
 }
