9e55255c3b5d3ba9ef51b7bf78d66c7db43a231d
ecourse <smkang@codenuri.co.kr>
2022-01-27 15:32:33 +0900

    Add files via upload

--- DAY4/5_인터페이스.cpp
+++ DAY4/5_인터페이스.cpp
@@ -1,19 +1,39 @@
 // 5_인터페이스
 #include <iostream>
 
+// 강한 결합(tightly coupling)
+
+// 하나의 클래스가 다른 클래스 사용시
+// 이름을 클래스 직접 사용하는것.
+
+// 교체 불가능한 경직된 디자인..
+
+
+
+
+
 class Camera
 {
 public:
 	void take() { std::cout << "take picture" << std::endl; }
 };
+class HDCamera
+{
+public:
+	void take() { std::cout << "take HD picture" << std::endl; }
+};
 class People
 {
 public:
-	void useCamera(Camera* p) { p->take(); }
+	void useCamera(Camera* p)   { p->take(); }
+	void useCamera(HDCamera* p) { p->take(); }
 };
 int main()
 {
 	People p;
 	Camera c;
 	p.useCamera(&c);
+
+	HDCamera hc;
+	p.useCamera(&hc); // ?
 }
