0724796f2ec7be924c5854d9e2fe3cda2ed1d9f2
ecourse <smkang@codenuri.co.kr>
2022-01-27 16:02:24 +0900

    Add files via upload

---
+++ DAY4/6_메뉴1.cpp
@@ -0,0 +1,20 @@
+// 6_메뉴1
+#include <iostream>
+#include <vector>
+#include <string>
+#include <conio.h>
+
+int main()
+{
+	printf("1. 김밥\n");
+	printf("2. 라면\n");
+	printf("3. 국수\n");
+	printf("메뉴를 선택하세요 >> ");
+	int cmd;
+	scanf_s("%d", &cmd);
+
+	switch (cmd)
+	{
+		// ....
+	}
+}
