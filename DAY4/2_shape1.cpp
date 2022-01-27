58cf12b9aae5efaf5d51bfffb67269042e5a51ed
ecourse <smkang@codenuri.co.kr>
2022-01-27 13:33:43 +0900

    Add files via upload

--- DAY4/2_shape1.cpp
+++ DAY4/2_shape1.cpp
@@ -1,7 +1,14 @@
-
 // 2_shape1.cpp  - 교재 152 page 부터 있는 예제 입니다.
 #include <iostream>
 #include <vector>
+
+// 1. 모든 도형을 타입화 하면 편리하다
+
+class Rect
+{
+public:
+	void draw() { std::cout << "Draw Rect" << std::endl; }
+};
 
 int main()
 {
